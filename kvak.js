9caa183e5448b280ef9ab7ce2d614ac898fe07c0
Eric Schrijver <eric@ericschrijver.nl>
2012-07-26 11:34:41 +0200

    Added a marker that can be used to geotag uploads, moved all JS to seperate file

--- kvak.js
+++ kvak.js
@@ -1,3 +1,30 @@
+var map;
+var uploadMarker;
+
+function initialize() {
+  var myOptions = {
+    zoom: 13,
+    center: new google.maps.LatLng(67.72694, -164.53333),
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    disableDefaultUI: true
+  };
+  map = new google.maps.Map(document.getElementById('map_canvas'),
+myOptions);
+
+  var myLatlng = new google.maps.LatLng(67.72694, -164.53333);
+  uploadMarker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"Hello World!",
+    draggable: true,
+    visible: false
+  });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 var showControls = function() {
     $(".kvak-ui.persistent").fadeIn()
 };
@@ -8,6 +35,9 @@ var hideControls = function() {
 
 var hideOtherScreenElements = function() {
     $(".kvak-ui.specific").hide();
+    if (typeof uploadMarker !== "undefined") {
+        uploadMarker.setVisible(false);
+    }
 }
 
 $("#map_canvas").click(function(e) {
@@ -22,16 +52,18 @@ $(".kvak-ui.trigger").click(function(e) {
     var target = $('#' + $(this).attr("rel"))
 
     if( target.is(":visible") ) {
-        target.hide();
+        hideOtherScreenElements();
     }
     else {
         hideOtherScreenElements();
         target.show();
+        if ( $(this).attr("id") === "kvak-upload-trigger" ) {
+            uploadMarker.setVisible(true);
+        }
     }
 });
 
-
-hideOtherScreenElements(); hideControls();
+hideOtherScreenElements();  hideControls();
 
 // opening
 
